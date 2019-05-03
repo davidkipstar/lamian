fa8e99e2dee84bfd0204785c2407bbaec357e691
davidkipstar <david.arnold93@gmail.com>
2019-05-03 19:53:33 +0200

    changed manager

--- async_Manager.py
+++ async_Manager.py
@@ -47,11 +47,11 @@ class Manager:
         # 'amount_to_sell': {'amount': 30397, 'asset_id': '1.3.1570'},
         #  'min_to_receive': {'amount': 384474522, 'asset_id': '1.3.3543'}, 
         
-        self.coin = Asset(self.buy)
-        self.coin_orders = []
+        #.coin = Asset(self.buy)
+        #self.coin_orders = []
 
-        if coin not in Manager.managers:
-            Manager.managers[coin] = self
+        #if self.coin not in Manager.managers:
+        #    Manager.managers[coin] = self
         
     def expired_or_not(self, order):
         #check if order is expired
