34f676c4d8ab41f71174a360ee6c3c428609cb1b
jgitr <julian.winkel@hu-berlin.de>
2019-06-09 21:39:51 +0200

    when price dumped, had alternating lifoprices. hopefully rounding up fixes this, but it remains untested as the avg price is now unavailable due to spam

--- utils.py
+++ utils.py
@@ -96,7 +96,7 @@ def find_price(orderbook, ob_th, tsize, avg_buy_price_lifo = 0, previous_order =
             opt_price_rounded = opt_price
 
     # Minimum price is our weighted buy price
-    lifoprice = Decimal(avg_buy_price_lifo).quantize(satoshi)
+    lifoprice = Decimal(avg_buy_price_lifo).quantize(satoshi, rounding=ROUND_UP)
 
     #print("Own revenue: {}".format(ownrevenue_v))
     #print("opt : {} , rounded: {}".format(opt_price, opt_price_rounded))
