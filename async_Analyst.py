0dc8195ff526d65c0b4a74844ee1ef418b9f1224
jgitr <julian.winkel@hu-berlin.de>
2019-06-16 14:55:25 +0200

    more coins, higher bal

--- async_Analyst.py
+++ async_Analyst.py
@@ -80,7 +80,7 @@ class Analyst:
         if kwargs['tradingside'] == 'buy':
             data['buy'] = coin 
             data['sell'] = self.major_coin
-            data['tsize'] = (0.5 * self.major_balance['amount'])/len(self.whitelist) # only invest half of our btc at a time
+            data['tsize'] = (0.8 * self.major_balance['amount'])/len(self.whitelist) # only invest half of our btc at a time
             data['th'] = 0.04
             data['ob_th'] = 1
 
