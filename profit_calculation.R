3e467bf8228d9cfe9f8959c4ad9f935c2d68bd99
J.W <julian.winkel@growney.de>
2019-06-21 16:39:49 +0200

    sbc

--- profit_calculation.R
+++ profit_calculation.R
@@ -9,10 +9,23 @@ calculate_profit = function(dat, blacklist = c('OPEN.BTC', 'XMR', 'BCO')) {
 
     sub = dat[!Asset.bought %in% blacklist & !Asset.sold %in% blacklist]
 
+    buy = sub[, .('btcbought' = sum(Amount.in.BTC), 'quotebought' = sum(Amount.received), 'meanprice' = mean(Price)), Asset.bought]
+    sell = sub[, .('btcsold' = sum(Amount.in.BTC), 'quotesold' = sum(Amount.paid)), Asset.sold]
+
+    m = merge(buy, sell, by.x = 'Asset.bought', by.y = 'Asset.sold')
+    m[, 'inventory' := quotebought - quotesold]
+    m[, 'inventory_value' := inventory * meanprice]
+
+    mm = m[Asset.bought != 'BTC']
+
     outl = list(
-                'sold' = sub[Asset.sold == 'BTC', sum(Amount.in.BTC)],
-                'bought' = sub[Asset.bought == 'BTC', sum(Amount.in.BTC)]
+                # Values in Bitcoin
+                'sold' = mm[, sum(btcsold)],
+                'bought' = mm[, sum(btcbought)],
+                'inventory_value' = mm[, sum(inventory_value)]
                 )
+
+    outl$profit = outl$sold - outl$bought + outl$inventory_value
 
     return(outl)
 }
